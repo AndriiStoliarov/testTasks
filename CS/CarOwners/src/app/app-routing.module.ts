057a9466bb8c4a44b519cd12e28ddfb910137767
LightWarrior <andrey159852@gmail.com>
2023-07-18 23:04:26 +0300

    files uploaded

---
+++ CS/CarOwners/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CarOwnersComponent, CruFormComponent } from './components';
+
+const routes: Routes = [
+  {
+    path: '', component: CarOwnersComponent, children: [
+      {path: '', redirectTo: 'records', pathMatch: 'full'},
+      {path: 'records', component: CruFormComponent},
+    ]
+  },
+  {
+    path: 'record', component: CruFormComponent
+  },
+  {
+    path: 'records/:id', component: CruFormComponent
+  },
+  {
+    path: 'records/:id/edit', component: CruFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
